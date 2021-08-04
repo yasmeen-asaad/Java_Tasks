da37507311501d8ac9dccda6111c527a5f573483
yasmeen asaad <46120393+yasmeen-asaad@users.noreply.github.com>
2021-08-04 05:35:44 +0200

    Update application.java

--- Titanic_task/src/main/java/application.java
+++ Titanic_task/src/main/java/application.java
@@ -1,14 +1,34 @@
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import joinery.DataFrame;
+import tech.tablesaw.*;
+import tech.tablesaw.api.Table;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-public class application
+/**
+ *
+ * @author yasmeen
+ */
+public class application 
 {
-    public static void main(String[] args) throws IOException {
-        File titanic_file = new File("D:\\ITI\\Java\\Tasks\\Titanic_task\\Titanic.csv");
-        List<String> lines = Files.readAllLines(titanic_file.toPath());
-
+    public static void main(String[] args) throws IOException
+    {
+         //---------------Reading Titanic Data Set Using Joinery Package ----------------
+        String path = "src/main/resources/data/titanic.csv";
+        DataFrame<Object> df_1 = DataFrame.readCsv(path);
+        System.out.println("A summary about Titanic data using joinery package");
+        System.out.println(df_1.describe());
+        System.out.println("------------------------------");
         
-
+        //---------------Reading Titanic Data Set Using Tablesaw Package ----------------
+        Table df_2 = Table.read().csv(path);
+        System.out.println("A summary about Titanic data using tablesaw package");
+        System.out.println(df_2.summary());
+  
     }
+    
+    
 }
